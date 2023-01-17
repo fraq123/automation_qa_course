01819bab6c8dfa5e3688946eb8bd4ea4c8275e9a
Кирилл <244egorov@gmail.com>
2023-01-17 20:54:30 +0500

    update elements tests added test radio button

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,6 @@
 import time
 
-from pages.elements_page import TestBoxPage, CheckBoxPage
+from pages.elements_page import TestBoxPage, CheckBoxPage, RadioButtonPage
 
 
 class TestElements:
@@ -23,4 +23,19 @@ class TestElements:
             input_checkbox = check_box_page.get_checked_checkboxes()
             output_checkbox = check_box_page.get_output_result()
             assert input_checkbox == output_checkbox, 'checkboxes gave not ben selected'
+
+    class TestRadioButton:
+
+        def test_radio_button(self, driver):
+            radio_button_page = RadioButtonPage(driver, 'https://demoqa.com/radio-button')
+            radio_button_page.open()
+            radio_button_page.click_on_the_radio_button('yes')
+            output_yes = radio_button_page.get_output_result()
+            radio_button_page.click_on_the_radio_button('impressive')
+            output_impressive = radio_button_page.get_output_result()
+            radio_button_page.click_on_the_radio_button('no')
+            output_no = radio_button_page.get_output_result()
+            assert output_yes == 'Yes', "'Yes' have not been selected"
+            assert output_impressive == 'Impressive', "'Impressive' have got been selected"
+            assert output_no != 'No', "'No' have got been selected"
 
