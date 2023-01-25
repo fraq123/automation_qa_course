fe91787b741e6a475143a90989127e586df62281
Кирилл <244egorov@gmail.com>
2023-01-25 15:59:36 +0500

    update elements tests added tests: - test_different_click_on_the_buttons

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-from pages.elements_page import TestBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage
+from pages.elements_page import TestBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonsPage
 
 
 class TestElements:
@@ -82,5 +82,18 @@ class TestElements:
             web_table_page.open()
             count = web_table_page.select_up_to_some_rows()
             assert count == [5, 10, 20, 25, 50, 100], 'The number of rows in the table has not been change incorrectly'
+
+    class TestButtonsPage:
+
+        def test_different_click_on_the_buttons(self, driver):
+            button_page = ButtonsPage(driver, 'https://demoqa.com/buttons')
+            button_page.open()
+            double = button_page.click_on_different_button('double')
+            right = button_page.click_on_different_button('right')
+            click = button_page.click_on_different_button('click')
+            assert double == "You have done a double click", "The double click button was not present"
+            assert right == "You have done a right click", "The right click button was not present"
+            assert click == "You have done a dynamic click", "The dynamic click button was not present"
+
 
 
